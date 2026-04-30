ff190d3d07d0c949d4abab9a0291c877f4e46f2a
pchatman <34121076+pchatman@users.noreply.github.com>
2026-04-30 08:28:25 -0400

    Update Todo, add imports, correct package errors

---
+++ todo-api/src/main/java/com/example/todo/model/Todo.java
@@ -0,0 +1,28 @@
+package com.example.todo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private boolean completed = false;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
